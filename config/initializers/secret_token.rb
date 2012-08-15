eddd2f5d849b7ff363de54fb61fef4d29c1a1cc1
Development <transfer@Jamess-iMac.local>
2012-08-15 21:02:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolidayCalendar::Application.config.secret_token = 'b6c853d2a3f74916bb2128e0f46728b823c7a1fd1a6abddce98e3fefa268e8e530a127ce75f63dd3cb1939594af19ce41931152c2556e8745ac65918b0f70d58'
